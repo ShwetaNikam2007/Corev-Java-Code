576e9e13da30a59e296ba24901c9e2f021fd1cc3
Nikam <shweta.b.nikam@capgemini.com>
2022-09-29 15:35:59 +0530

    Addition of two numbers

---
+++ addition.java
@@ -0,0 +1,14 @@
+//Java code - addition of two numbers
+
+
+class addition{
+
+public static void main(string args){
+
+	int a = 10;
+	int b = 20;
+	int result;
+	result = a + b ;
+	System.out.println("Addition is : "+result);
+}
+}
